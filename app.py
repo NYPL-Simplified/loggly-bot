82838c8527eb4124dc738052f103e6211d100b50
Amy Slagle <amyslagle@nypl.org>
2016-01-19 12:58:49 -0500

    Changed request method to POST, and improved exception handling. (Fixes #1)

--- app.py
+++ app.py
@@ -2,22 +2,44 @@ import os
 from flask import Flask, request
 import requests
 import json
+import traceback
+import re
 from pdb import set_trace
 
 app = Flask(__name__)
 
-@app.route("/log")
+@app.route("/log", methods=["POST"])
 def log():
-    webhook_url = os.environ['WEBHOOK_URL']
-    alert_name = request.args.get('alert_name')
-    num_hits = request.args.get('num_hits')
-    recent_hits = request.args.get('recent_hits')
-    search_link = request.args.get('search_link')
-    text = "%s: <%s|%s hits>, including:" % (alert_name, search_link, num_hits)
-    fields = [{'value': hit['message'], 'short':True} for hit in json.loads(recent_hits)]
-    attachment = {"fallback":text, "pretext":text, "fields": fields}
-    payload = json.dumps({"attachments":[attachment]})
-    requests.post(webhook_url, data={'payload':payload})
+    try:
+        webhook_url = os.environ['WEBHOOK_URL']
+        data = json.loads(request.data)
+        alert_name = data.get('alert_name')
+        num_hits = data.get('num_hits')
+        recent_hits = data.get('recent_hits')
+        search_link = data.get('search_link')
+        text = "%s: <%s|%s hits>" % (alert_name, search_link, num_hits)
+        try:
+            hits = json.loads(recent_hits)
+            fields = [{'value': hit['message'], 'short':True} for hit in hits]
+        except Exception as e:
+            # The recent hits might not be valid json, so we'll just try to find them in the string..
+            # This happens in loggly when there's a traceback in the hit.
+            recent_hits_re = re.compile("\"recent_hits\"\s:(.*)")
+            hits = recent_hits_re.search(request.data)
+            if hits:
+                fields = [{'value': hits.group(1)}]
+            else:
+                # Didn't find any recent hits in the request, so we'll just send the whole thing.
+                fields = [{'value': request.data}]
+        attachment = {"fallback":text, "pretext":text, "fields": fields}
+        payload = json.dumps({"attachments":[attachment]})
+        requests.post(webhook_url, data={'payload':payload})
+    except Exception as e:
+        message = "Sorry, there was an alert from loggly but I couldn't process it: %s" % e
+        payload = json.dumps({"attachments": [{"fallback": message, "pretext": message, "fields": []}]})
+        print e
+        traceback.print_exc()
+        requests.post(webhook_url, data={'payload':payload})
     return ""
 
 
